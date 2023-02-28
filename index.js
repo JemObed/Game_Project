f6cc7af19ebc00f73ba624e65c5e39a2ad15b568
JemObed <jemhernandez01@gmail.com>
2023-02-28 16:03:11 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/*road map from notes
+1.) Character selcetion through Dom interactivity
+2.) Classes/Objects for 4 characters 
+-Properties
+-Hp
+-Def
+-speed
+3.) Transition between selection into 1v1 combat through event sequence
+4.) Give  each character 4 attacks to select form
+5.) Damagae calculation
+-Cal formula (check for speed, queue for 2nd attack, then check accuracy if pass Foe_Hp-(Attack_Power-Foe_Def)=New_Foe_Hp/) 
+6.) Sequence for new turn
+7.) Repeat until Hp reaches value < 0 
+8.) Loser state Hp reaches 0
+9.) Winner state Player who's Attack reduces Foe_Hp to 0
+*/
